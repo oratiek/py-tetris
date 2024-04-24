0acab41253be12f7376fb5a00da07a7f3bfaa61b
oratiek <oratiek522@gmail.com>
2024-04-24 09:35:16 +0900

    delete pyxapp

---
+++ tetris/block.py
@@ -0,0 +1,87 @@
+import pyxel
+import configparser 
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+BLOCK_SIZE = int(config["BLOCK"]["SIZE"])
+ROW_SIZE = int(config["FIELD"]["ROW_SIZE"])
+COL_SIZE = int(config["FIELD"]["COL_SIZE"])
+X_OFFSET = int(config["FIELD"]["X_OFFSET"])
+Y_OFFSET = int(config["FIELD"]["Y_OFFSET"])
+
+class Block:
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.size = BLOCK_SIZE
+        self.color = color
+        self.fixed = False
+
+    def fix(self):
+        self.fixed = True
+
+    def go_down(self) -> None:
+        self.row += 1 
+
+    def collide_bellow(self, field) -> None:
+        if self.collide_bellow_wall():
+            #print("bellow_wall", True)
+            return True
+        elif self.collide_bellow_block(field):
+            #print("bellow_block", True)
+            return True
+        return False
+
+    def collide_bellow_wall(self) -> None:
+        if self.row == ROW_SIZE - 1:
+            return True
+        return False
+
+    def collide_bellow_block(self, field) -> None:
+        # FIXME インデックスエラーの可能性あり。collide_bellowで短絡評価されてここに来るときはindexerrorにならない場所のはずなんだけどな
+        bellow_block = field.get_block(self.row + 1, self.col) 
+        return bellow_block != 0
+    
+    # COLLIDE RIGHT
+    def collide_right(self, field) -> None:
+        if self.collide_right_wall() or self.collide_right_block(field):
+            return True
+        return False
+
+    def collide_right_wall(self) -> None:
+        return self.col >= COL_SIZE - 1
+    
+    def collide_right_block(self, field) -> None:
+        right_block = field.get_block(self.row, self.col + 1)
+        return right_block != 0 # TODO マジックナンバーの解消
+
+    def go_right(self) -> None:
+        self.col += 1
+
+    # COLLIDE LEFT
+    def collide_left(self, field) -> None:
+        if self.collide_left_wall() or self.collide_left_block(field):
+            return True
+        return False
+
+    def collide_left_wall(self) -> None:
+        return self.col <= 0
+
+    def collide_left_block(self, field) -> None:
+        left_block = field.get_block(self.row, self.col - 1)
+        return left_block != 0 # TODO マジックナンバー解消
+    
+    def go_left(self) -> None:
+        self.col -= 1
+
+    def draw(self) -> None:
+        x = X_OFFSET + (self.col * self.size)
+        y = Y_OFFSET + (self.row * self.size)
+        pyxel.rect(x, y, self.size, self.size, self.color)
+
+    def draw_by_index(self, row, col):
+        x = X_OFFSET + (col * self.size)
+        y = Y_OFFSET + (row * self.size)
+        pyxel.rect(x, y, self.size, self.size, self.color)
+
---
+++ tetris/tetris.py
@@ -0,0 +1,53 @@
+import pyxel
+import time
+import sys
+
+from mino import Mino
+from field import Field
+
+class App:
+    def __init__(self):
+        pyxel.init(200,200, fps=10)
+
+        # initialize Field
+        self.field = Field()
+        self.mino = Mino()
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.field.check()
+
+        # controller
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.mino.go_right(self.field)
+        elif pyxel.btn(pyxel.KEY_LEFT):
+            self.mino.go_left(self.field)
+        
+        if pyxel.btn(pyxel.KEY_UP):
+            self.mino.rotate_right(self.field)
+        elif pyxel.btn(pyxel.KEY_DOWN):
+            self.mino.rotate_left(self.field)
+
+        # mino go down
+        self.mino.go_down(self.field)
+        
+        # check floor collision
+        if self.mino.collide_bellow(self.field):
+            # fix to field 
+            self.field.fix_mino(self.mino) # fieldにminoを渡す
+
+            # check and clear lines
+            self.field.clear_lines()
+            
+            # renew mino
+            self.mino = Mino()
+   
+    def draw(self):
+        pyxel.cls(0)
+
+        self.field.draw()
+        self.mino.draw()
+
+if __name__ == "__main__":
+    App()
